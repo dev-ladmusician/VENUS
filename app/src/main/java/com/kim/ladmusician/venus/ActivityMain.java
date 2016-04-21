f941cdc8c809402c7051bc075983411e05ef8779
dev-ladmusician <dev-ladmusician@gmail.com>
2016-04-21 22:10:17 +0900

    init activitytMain for test

--- app/src/main/java/com/kim/ladmusician/venus/ActivityMain.java
+++ app/src/main/java/com/kim/ladmusician/venus/ActivityMain.java
@@ -1,13 +1,78 @@
 package com.kim.ladmusician.venus;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.support.v7.app.AppCompatActivity;
+
+import com.kim.ladmusician.venus.util.LogUtil;
+
+import java.util.ArrayList;
 
 public class ActivityMain extends AppCompatActivity {
+    private final String TAG = "ACTIVITY_MAIN";
+    private SpeechRecognizer mRecognizer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, getPackageName());
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "ko-KR");
+
+        mRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
+        mRecognizer.setRecognitionListener(listener);
+        mRecognizer.startListening(intent);
     }
+
+    private RecognitionListener listener = new RecognitionListener() {
+        @Override
+        public void onRmsChanged(float rmsdB) {
+            // TODO Auto-generated method stub
+        }
+        @Override
+        public void onResults(Bundle results) {
+            String key = "";
+            key = SpeechRecognizer.RESULTS_RECOGNITION;
+            ArrayList<String> mResult = results.getStringArrayList(key);
+            String[] rs = new String[mResult.size()];
+            mResult.toArray(rs);
+            LogUtil.print(TAG, rs[0]);
+        }
+        @Override
+        public void onReadyForSpeech(Bundle params) {
+            LogUtil.print(TAG, "READY FOR SPEECH");
+            // TODO Auto-generated method stub
+        }
+        @Override
+        public void onPartialResults(Bundle partialResults) {
+            LogUtil.print(TAG, "PARTIAL RESULT");
+        }
+        @Override
+        public void onEvent(int eventType, Bundle params) {
+            // TODO Auto-generated method stub
+        }
+        @Override
+        public void onError(int error) {
+            LogUtil.print(TAG, "ERROR");
+        }
+        @Override
+        public void onEndOfSpeech() {
+            LogUtil.print(TAG, "END OF SPEECH");
+        }
+
+        @Override
+        public void onBufferReceived(byte[] buffer) {
+            // TODO Auto-generated method stub
+        }
+
+        @Override
+        public void onBeginningOfSpeech() {
+            LogUtil.print(TAG, "BEGINNING OF SPEECH");
+        }
+    };
 }
--- app/src/main/java/com/kim/ladmusician/venus/util/LogUtil.java
+++ app/src/main/java/com/kim/ladmusician/venus/util/LogUtil.java
@@ -1,13 +1,14 @@
 package com.kim.ladmusician.venus.util;
 
-import android.support.graphics.drawable.BuildConfig;
 import android.util.Log;
+
+import com.kim.ladmusician.venus.BuildConfig;
 
 /**
  * Created by ladmusician on 4/21/16.
  */
 public class LogUtil {
     public static void print(String tag, String msg) {
-        if(BuildConfig.DEBUG) Log.e(tag, msg);
+        if(BuildConfig.isDebug) Log.e(tag, msg);
     }
 }
